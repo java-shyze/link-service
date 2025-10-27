c2b920749b01c93b52715e7fcc5306807f87fd77
Artyom <AMSmolin@yandex.ru>
2025-10-28 01:27:28 +0300

    Initial commit

---
+++ src/test/java/com/shyze/linkservice/LinkserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shyze.linkservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
